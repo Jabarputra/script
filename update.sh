ac5bf61a8f1ce962123cd2ab72e570900377fac8
Jabarputra <160265487+Jabarputra@users.noreply.github.com>
2024-06-21 17:58:08 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -48,12 +48,17 @@ res1() {
     rm -rf /usr/local/sbin/*~
     rm -rf /usr/local/sbin/gz*
     rm -rf /usr/local/sbin/*.bak
+    rm -rf /usr/local/sbin/m-noobz
+    wget /usr/local/sbin "https://raw.githubusercontent.com/Jabarputra/script/main/Cfg/m-noobz" 
+    cp m-noobz /usr/local/sbin
+    rm m-noobz*
+    chmod +x /usr/local/sbin/m-noobz
 }
 netfilter-persistent
 clear
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m             UPDATE SCRIPT        \e[0m"
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \e[1;97;101m          UPDATE SCRIPT        \e[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e "  \033[1;91m update script service\033[1;37m"
 fun_bar 'res1'
