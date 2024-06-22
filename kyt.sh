52f1ef9bb15f5eb99cbb76ce5cddf0885b1b8f37
Jabarputra <160265487+Jabarputra@users.noreply.github.com>
2024-06-22 16:20:27 +0700

    Add files via upload

--- kyt.sh
+++ kyt.sh
@@ -31,9 +31,9 @@ pip3 install -r kyt/requirements.txt
 #isi data
 echo ""
 figlet  Jabar shop  | lolcat
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          ADD BOT PANEL          \e[0m"
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[1;97;101m               ADD BOT PANEL           \e[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "${grenbo}Tutorial Creat Bot and ID Telegram${NC}"
 echo -e "${grenbo}[*] Creat Bot and Token Bot : @BotFather${NC}"
 echo -e "${grenbo}[*] Info Id Telegram : @MissRose_bot , perintah /info${NC}"
